ba198875c41aad337a9d790d196cf21f7dcd4f0f
lipinsks <szymlip725@gmail.com>
2025-04-17 16:04:37 +0200

    improvement

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ def index():
 
 @app.route('/api/simulate')
 def simulate():
-    # read picks from query string
     picks = request.args.getlist('picks', type=int)
 
     def generate():
@@ -20,14 +19,18 @@ def simulate():
         while True:
             counter += 1
             draw = random.sample(range(1, MAX_NUMBER + 1), BALL_COUNT)
+
             if counter % 100000 == 0:
-                 yield f"data: {json.dumps({'counter': counter, 'drawPreview': draw})}\n\n"
+                cost = counter * COST_PER_DRAW
+                yield f"data: {json.dumps({'counter': counter, 'drawPreview': draw, 'cost': cost})}\n\n"
+
             if set(draw) == set(picks):
-                total_cost = counter * COST_PER_DRAW
-                yield f"data: {json.dumps({'counter': counter,'cost': total_cost,'draw': draw})}\n\n"
+                cost = counter * COST_PER_DRAW
+                yield f"data: {json.dumps({'counter': counter, 'draw': draw, 'cost': cost})}\n\n"
                 break
 
     return Response(generate(), mimetype='text/event-stream')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2137)
